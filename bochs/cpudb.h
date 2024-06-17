768a05d05ab58a7e75ea1a38ca45ae10edf4b719
Jasmine Iwanek <jasmine@iwanek.co.uk>
2024-06-17 01:30:43 -0400

    Add Intel i7-2700k (Preliminary)

--- bochs/cpudb.h
+++ bochs/cpudb.h
@@ -42,6 +42,7 @@ bx_define_cpudb(core2_penryn_t9600)
 bx_define_cpudb(corei5_lynnfield_750)
 bx_define_cpudb(corei5_arrandale_m520)
 bx_define_cpudb(corei7_sandy_bridge_2600k)
+bx_define_cpudb(corei7_sandy_bridge_2700k)
 #if BX_SUPPORT_AVX
 bx_define_cpudb(zambezi)
 bx_define_cpudb(trinity_apu)
